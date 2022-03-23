c648554d14935c08e77dcd37a50f46878c55075f
br <bartosz.roguski@4soft.co>
2022-03-23 17:38:49 +0100

    Track base-vanilla model with DVC

--- poetry_generation/models/empath_lightning.py
+++ poetry_generation/models/empath_lightning.py
@@ -5,13 +5,12 @@ import pytorch_lightning as pl
 from poetry_generation.models.modules.emotion_classifier import RobertaEmpathModel
 
 
-class EmpathModuleLightning(pl.LightningModule):
+class EmpathLightningModel(pl.LightningModule):
     def __init__(
         self,
         model_name_or_path: str,
-        train_path: str,
-        dev_path: str,
-        test_path: str,
+        learning_rate: float,
+        warmup_steps: int,
         n_classes: int = 3,
     ):
         super().__init__()
